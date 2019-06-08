a1b602a2acdac8218ab694c16f7e42e0560b4a1a
Veer-Tech Trendings <49147903+speedyj007@users.noreply.github.com>
2019-06-08 23:18:41 +0530

    The Main File

---
+++ exampractise_3.java
@@ -0,0 +1,30 @@
+
+public class exampractise_3{
+	public static void main(String []args)
+	{
+		int i = 0;
+		
+		
+		
+		System.out.println();
+		
+		for(int j = 0;j<10;j++ )
+		{
+		i++;
+		for(int z = 1;z<=10;z++)
+		{
+			
+		
+			int sum = i*z;
+			System.out.println(" "+i+" * "+z+" = "+sum );
+			
+			
+		}
+		
+		System.out.println("-----------------------------");
+		System.out.println();
+		}
+		System.out.println();
+		
+	}
+}
